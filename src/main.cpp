31b41726fd6147f8dbc41020126bfdfc6dd899f7
InDigitalEx <indigitale8@gmail.com>
2019-05-24 21:59:01 +0400

    Add gamemode init

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,34 @@
+// Includes
 #include <stdio.h>
 #include <string.h>
-
 #include <plugincommon.h>
 #include <sampgdk.h>
+#include <time.h>
+
+#include "CCore.h"
 
 using sampgdk::logprintf;
 
+// Callbacks
+PLUGIN_EXPORT bool PLUGIN_CALL OnGameModeInit() {
+	auto lasttime = GetTickCount();
+
+	logprintf("[Gamemode] »гровой режим был успешно загружен за <%d> мсек",
+		GetTickCount()-lasttime);
+	#ifdef WIN32
+	logprintf("¬рем€ компил€ции: %s %s", __DATE__, __TIME__);
+	#endif
+
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnGameModeExit() {
+	auto lasttime = GetTickCount();
+
+	logprintf("[Gamemode] »гровой режим был успешно выгружен за <%d> мсек",
+		GetTickCount() - lasttime);
+	return true;
+}
 
 // Plugin ensures
 PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports() {
